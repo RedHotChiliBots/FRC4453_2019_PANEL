4b03fdca5afcff551aa6b738c6a6e90196d8eb3d
Steve Seifert <sdseifert@oc8880207636.ibm.com>
2019-03-02 13:55:18 -0500

    Add SystemCtl service

--- misc/ButtonBoard.sh
+++ misc/ButtonBoard.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 export JAVA_HOME=/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt
-JVMARGS="-Dlog4j2.configurationFile=/home/pi/ButtonBoard/log4j2.xml"
-sudo $JAVA_HOME/bin/java $JVMARGS -jar /home/pi/ButtonBoard/ButtonBoard.jar
+sudo $JAVA_HOME/bin/java -jar /home/pi/ButtonBoard/ButtonBoard.jar
